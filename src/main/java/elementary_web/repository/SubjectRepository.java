4e6abdbe20b2c92d06c4e0e4dd8a2d744abbbdb3
LAPTOP USA PRO <LAPTOP USA PRO@192.168.1.4>
2022-05-23 18:17:29 +0700

    findSubjectBySubjectNameOrSubjectCode

--- src/main/java/elementary_web/repository/SubjectRepository.java
+++ src/main/java/elementary_web/repository/SubjectRepository.java
@@ -1,11 +1,15 @@
 package elementary_web.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import elementary_web.entity.Account;
 import elementary_web.entity.Subject;
 
 @Repository
 public interface SubjectRepository extends JpaRepository<Subject, Integer> {
-
+	@Query(value = "SELECT * FROM MON_HOC WHERE (EMAIL = ?1) OR (MA_MON_HOC = ?1) OR (TEN_MON_HOC = ?1)", nativeQuery = true)
+	Account findSubjectBySubjectNameOrSubjectCode(String SubjectNameOrSubjectCode);
+	
 }
